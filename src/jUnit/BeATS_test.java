6fabc4d025525b2434d11d22ecadc191a99a7338
Samitha Samaranayake <samitha@gmail.com>
2013-11-20 14:26:49 -0800

    added BeATS test

---
+++ src/jUnit/BeATS_test.java
@@ -0,0 +1,37 @@
+package jUnit;
+
+import edu.berkeley.path.beats.jaxb.FundamentalDiagramSet;
+import edu.berkeley.path.beats.jaxb.RouteSet;
+import edu.berkeley.path.beats.simulator.DemandSet;
+import edu.berkeley.path.beats.simulator.Link;
+import edu.berkeley.path.beats.simulator.ObjectFactory;
+import edu.berkeley.path.beats.simulator.Scenario;
+
+public class BeATS_test {
+
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String configfilename = "/Users/Samitha/Documents/github/DTA-PC/graphs/Rerouting_sent_newxsd_v2.xml";
+			Scenario scenario;
+			try {
+				scenario = ObjectFactory.createAndLoadScenario(configfilename);
+		
+			
+			FundamentalDiagramSet fdset = scenario.getFundamentalDiagramSet();
+			DemandSet demset = (DemandSet) scenario.getDemandSet();
+			
+			RouteSet route_set = scenario.getRouteSet();
+			
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+	}
+
+}
